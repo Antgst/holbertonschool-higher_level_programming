bf66229743e05de0b8cb3f5c2210a9f8167d4eec
Antgst <ant.gousset@gmail.com>
2026-01-13 23:56:24 +0100

    add: add T8 and fix 8-main.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    return ord('A') <= ord(c) <= ord('Z')
+    final = ""
+    for i in str:
+        if 'a' <= i <= 'z':
+            final += chr(ord(i) - 32)
+        else:
+            final += i
+    print("{}".format(final))
