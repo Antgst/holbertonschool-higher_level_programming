1aff76095d82cfb28eeabcd8194670f0dd15ebd9
Antgst <ant.gousset@gmail.com>
2026-01-15 15:22:39 +0100

    add: add T2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
 
+import sys
+
+if __name__ == "__main__":
+    argc = len(sys.argv) - 1
+
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1"" argument:".format(sys.argv[1]))
+        print("1"" ""{}".format(sys.argv[1]))
+    else:
+        print("{}"": ""{}".format(argc, sys.argv[1:]))
+        for i, args in enumerate(sys.argv[1:], 1):
+            print("{}: {}".format(i, args))
