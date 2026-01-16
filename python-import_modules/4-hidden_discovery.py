5de50cc96e9783a009b6ce5d3caf80568ea6a301
Antgst <ant.gousset@gmail.com>
2026-01-16 10:10:59 +0100

    add: adda T4

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == __import__("hidden_4")
+    words = __import__("hidden_4")
+
+    name = dir(hidden)
+
+    for name in name:
+        if not name.startswitch("__"):
+            print(name)
