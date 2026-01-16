b515639aa87074f3735543a2f73c23a79eeb0d4e
Antgst <ant.gousset@gmail.com>
2026-01-16 10:36:11 +0100

    fix: T3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-
 import sys
 
 if __name__ == "__main__":
-    add = sum(int(args) 
-        for args in sys.argv[1:])
-    print(add)
+    print(sum(int(arg) for arg in sys.argv[1:]))
