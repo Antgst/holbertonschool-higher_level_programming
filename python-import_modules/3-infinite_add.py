3746fae70134fd75aca04a4e4906ff41f48d251c
Antgst <ant.gousset@gmail.com>
2026-01-16 09:48:57 +0100

    fix: fix pycodestyle T3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@
 import sys
 
 if __name__ == "__main__":
-    add = sum(int(args) 
-    for args in sys.argv[1:])
-    print (add)
+    add = sum(int(args)
+        for args in sys.argv[1:])
+            print(add)
