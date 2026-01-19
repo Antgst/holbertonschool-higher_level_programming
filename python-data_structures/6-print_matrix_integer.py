e0a151c95f32d555cb449336e2294be6d81523fa
Antgst <ant.gousset@gmail.com>
2026-01-19 15:49:22 +0100

    add: add T6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print()
+        return
     for i in matrix:
-        print("{}".format(i))
-        for j in i:
-            print("{}".format(j))
+        for j in range(len(i)):
+            if j != len(i) - 1:
+                print("{}".format(i[j]), end=" ")
+            else:
+                print("{}".format(i[j]))
