63770bff038bb26845c35c1b7003e486e824ca33
Antgst <ant.gousset@gmail.com>
2026-01-19 12:52:40 +0100

    add: add T5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def no_c(my_string):
+    if not my_string:
+        return ""
+    newstring = ""
+    for ch in my_string:
+        if ch != 'C' and ch != 'c':
+            newstring += ch
+    return newstring
