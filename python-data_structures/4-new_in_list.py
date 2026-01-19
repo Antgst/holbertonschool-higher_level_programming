194138d4f03117d979debd22ef67f33eaaad824b
Antgst <ant.gousset@gmail.com>
2026-01-19 12:07:07 +0100

    add: add T4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    copyoflist = my_list[0:]
+    if idx < 0 or idx >= len(my_list):
+        return copyoflist
+    copyoflist[idx] = element
+    return copyoflist
