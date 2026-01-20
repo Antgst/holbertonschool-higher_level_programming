f52d89fb3c02b09d4775e01322a69b7d4367c919
Antgst <ant.gousset@gmail.com>
2026-01-20 13:58:03 +0100

    add: resolve T0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    i = 0
+    count = 0
+
+    if my_list is None:
+        print()
+        return 0
+
+    if not my_list:
+        print()
+        return 0
+
+    while i < x:
+        try:
+            print(my_list[i], end="")
+            i += 1
+            count += 1
+        except IndexError:
+            break
+
+    print()
+    return count
