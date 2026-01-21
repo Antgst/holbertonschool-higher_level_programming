63879fc0d0cf9fc6bac488b69d4bb16bcde3653a
Antgst <ant.gousset@gmail.com>
2026-01-21 13:42:55 +0100

    fix: syntax of T6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix == [[]]:
-        print()
-        return
-    for i in matrix:
-        for j in range(len(i)):
-            if j != len(i) - 1:
-                print("{}".format(i[j]), end=" ")
+    for row in matrix:
+        for j in range(len(row)):
+            if j != len(row) - 1:
+                print("{:d}".format(row[j]), end=" ")
             else:
-                print("{}".format(i[j]))
+                print("{:d}".format(row[j]), end="")
+        print()
