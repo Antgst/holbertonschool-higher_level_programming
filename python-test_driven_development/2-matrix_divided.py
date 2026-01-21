5fd58f7653bf4d9c0b428f1ab910655e01821251
Antgst <ant.gousset@gmail.com>
2026-01-21 13:04:00 +0100

    fix: syntax of T1

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -18,15 +18,16 @@ def matrix_divided(matrix, div):
     if (
         not isinstance(matrix, list)
         or matrix == []
-        or any(not isinstance(row, list) or row == [] for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of "
-                        "integers/floats")
+        or any(not isinstance(row, list) or row == [] for row in matrix)
+    ):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of "
+            "integers/floats"
         )
     row_len = len(matrix[0])
     for row in matrix:
         if len(row) != row_len:
-            raise TypeError("Each row of the matrix
-                            "must have the same size")
+            raise TypeError("Each row of the matrix must have the same size")
         for item in row:
             if not isinstance(item, (int, float)):
                 raise TypeError("matrix must be a matrix (list of lists) of "
