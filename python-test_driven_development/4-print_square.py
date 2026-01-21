0c26ec7f1571b585848279798f7c8f175b00bb23
Antgst <ant.gousset@gmail.com>
2026-01-21 13:22:08 +0100

    add: add T3

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
+"""This module provides a function that prints a
+square using the # character."""
+
+
 def print_square(size):
+    """Print a square of size x size using #.
+
+    Raises TypeError if size is not an integer.
+    Raises ValueError if size is less than 0.
+    """
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
