2ccc274e7d38633756bc93780e760ca08e5250f6
Antgst <ant.gousset@gmail.com>
2026-01-21 14:02:30 +0100

    fix: syntax of T0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,14 +3,6 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     count = 0
 
-    if my_list is None:
-        print()
-        return 0
-
-    if not my_list:
-        print()
-        return 0
-
     while i < x:
         try:
             print(my_list[i], end="")
