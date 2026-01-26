c9301c7a17618d07a6c698f340a729c71b98b35a
Antgst <ant.gousset@gmail.com>
2026-01-26 11:06:00 +0100

    add: resolve T2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,5 +1,27 @@
 #!/usr/bin/python3
 
-'''TEST DOC'''
+'''Square module, this module defines a Square class with size validation'''
 
-# TEST DOC
+
+class Square:
+    '''
+    Defines a square by its size
+
+    Attributes:
+    _size (int): Size of the square (private)
+    '''
+    def __init__(self, size=0):
+        '''Initialize a new Square
+
+        Args:
+        Size (int): Size of the square. Default to 0
+
+        Raises:
+        TypeError: If size is not an integer
+        ValueError: If size is less than 0
+        '''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
