8cb027d322fd4ec7aa244dd7da724f6863b1dd3b
Antgst <ant.gousset@gmail.com>
2026-01-26 13:15:09 +0100

    fix: edit docstring of T3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
 
-'''Square module, this module defines a Square'''
+'''Square module.
+This module defines a Square class with size validation and an area method.
+'''
 
 
 class Square:
+    '''Defines a square by its size.
+
+    Attributes:
+    __size (int): Size of the square (private)
+    '''
 
     def __init__(self, size=0):
+        '''Initialize a new Square.
+
+        Args:
+        size (int): Size of the square. Defaults to 0.
+
+        Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+        '''
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
@@ -13,4 +29,9 @@ class Square:
         self.__size = size
 
     def area(self):
+        '''Compute and return the area of the square.
+
+        Returns:
+        int: The area of the square (size * size)
+        '''
         return (self.__size * self.__size)
