f71adace1dc9f3483dfbfa0e023fd6ad6fcfdd47
Antgst <ant.gousset@gmail.com>
2026-02-02 21:33:44 +0100

    add: resolve T4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
-'''
-Docstring pour python-inheritance.4-inherits_from
-'''
+"""4-inherits_from
+Defines a function to check whether an object inherits from a specified class.
+"""
 
 
 def inherits_from(obj, a_class):
-    ''''''
-    return isinstance(obj, a_class) is not type(obj, a_class)
+    """Return True if obj is an instance of a subclass
+    of a_class; otherwise False.
+
+    The result is False when obj is exactly an instance of a_class.
+
+    Args:
+        obj: Any Python object to test.
+        a_class: The class to compare against.
+
+    Returns:
+        bool: True if type(obj) is a subclass of a_class; otherwise False.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
