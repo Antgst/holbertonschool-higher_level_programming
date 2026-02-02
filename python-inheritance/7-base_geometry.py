6b2b23a982efc2d31f064041b25153e84bc2ac9e
Antgst <ant.gousset@gmail.com>
2026-02-02 21:46:22 +0100

    add: resolve T7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""6-base_geometry
+Defines a BaseGeometry class with an unimplemented area() method.
+"""
+
+
+class BaseGeometry:
+    """Base class for geometry objects."""
+    def area(self):
+        """Raise an Exception because area() is not implemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """"""
+        if type(value) is not int:
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
