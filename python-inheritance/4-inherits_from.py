261a8b1e6902a0c999e6747ba4d8027bd6321ded
Antgst <ant.gousset@gmail.com>
2026-02-02 15:49:51 +0100

    add: T4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+'''
+Docstring pour python-inheritance.4-inherits_from
+'''
+
+
+def inherits_from(obj, a_class):
+    ''''''
+    return isinstance(obj, a_class) is not type(obj, a_class)
