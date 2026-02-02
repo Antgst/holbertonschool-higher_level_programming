9af393a6b8bbfe9536ebae41d9e50bf3bc37e90c
Antgst <ant.gousset@gmail.com>
2026-02-02 21:40:09 +0100

    add: resolve T6

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""6-base_geometry
+Defines a BaseGeometry class with an unimplemented area() method.
+"""
+
+
+class BaseGeometry:
+    """Base class for geometry objects."""
+    def area(self):
+        """Raise an Exception because area() is not implemented."""
+        raise Exception("area() is not implemented")
