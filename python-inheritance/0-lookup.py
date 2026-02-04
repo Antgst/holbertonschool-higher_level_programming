3080f3a2acd507aaf8448572f57cb4de1f633e42
Antgst <ant.gousset@gmail.com>
2026-02-04 10:02:10 +0100

    fix: T1

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-'''Defines a utility function to list an object's
+"""Defines a utility function to list an object's
 available attributes and methods.
 
 This module provides the `lookup` function,
 which returns a list of attribute and
 method names accessible on a given object.
-'''
+"""
 
 
 def lookup(obj):
-    '''Return the list of available attributes and methods of an object.
+    """Return the list of available attributes and methods of an object.
 
     Args:
         obj: Any Python object to introspect.
@@ -20,5 +20,5 @@ def lookup(obj):
     Example:
         >>> lookup([1, 2, 3])  # doctest: +ELLIPSIS
         ['__add__', ..., 'append', ...]
-        '''
+        """
     return dir(obj)
