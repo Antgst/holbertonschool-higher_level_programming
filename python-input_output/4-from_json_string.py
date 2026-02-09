b1895e6af03626f4f8bb6e5de6d89a9dda772fbf
Antgst <ant.gousset@gmail.com>
2026-02-09 11:28:36 +0100

    add: resolve T4

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
+'''Module that provides a function to convert a JSON
+string into a Python object.'''
+
+import json
+
 
 def from_json_string(my_str):
+    '''
+    Return the Python object represented by a JSON string.
+
+    Args:
+        my_str (str): JSON-formatted string.
+
+    Returns:
+        object: The Python data structure represented by the JSON string.'''
+    return json.loads(my_str)
