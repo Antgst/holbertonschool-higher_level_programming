011944233965184c57be8442b977c601c59ef55a
Antgst <ant.gousset@gmail.com>
2026-02-09 09:12:14 +0100

    add: test add

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,13 @@
+#!usr/bin/env python3
+"""
+Docstring pour python-abc.task_03_countediterator
+"""
+
+
+class CountedIterator:
+    """
+    Docstring pour CountedIterator
+    """
+    def __init__(self, iterable):
+        self.iterable = 0
+        self.
