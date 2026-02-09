96dca1a9473a2833fff3fda51cdd77f45a294e0b
Antgst <ant.gousset@gmail.com>
2026-02-09 11:21:32 +0100

    add: resolve T3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+'''Defines a function that returns the JSON
+representation of an object.'''
+
+import json
+
 
 def to_json_string(my_obj):
+    '''Return the JSON representation of my_obj (as a string).'''
+    return json.dumps('my_obj')
