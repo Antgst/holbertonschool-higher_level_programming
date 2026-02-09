18aca1846a7188350184f282deffa7e17f27c73a
Antgst <ant.gousset@gmail.com>
2026-02-09 10:46:29 +0100

    add: resolve T1

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+'''Defines a function that reads a UTF-8 text file and
+prints it to stdout.'''
+
 
 def read_file(filename=""):
+    '''Read a UTF-8 text file and print its content to stdout.'''
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
