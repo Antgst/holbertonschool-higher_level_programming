fbbdbd42cb31ecffc2e0cade0835a6d9717360d3
Antgst <ant.gousset@gmail.com>
2026-02-10 11:09:02 +0100

    add: resolve T0

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
+
+import json
+
+
 def serialize_and_save_to_file(data, filename):
-    # Your code here to serialize and save data to the specified file
-    pass
+    with open(filename, 'w', encoding="utf-8") as f:
+        json.dump(data, f)
 
 
 def load_and_deserialize(filename):
-    # Your code here to load and deserialize data from the specified file
-    pass
+    with open(filename, 'r', encoding="utf-8") as f:
+        return json.load(f)
