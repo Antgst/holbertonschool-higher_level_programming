ccfe09dcaba983c98b75462b6831c1d6c3599ea6
Antgst <ant.gousset@gmail.com>
2026-02-10 13:09:27 +0100

    fix: edit docstrings to T9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
+'''Module that defines a Student class
+with a JSON-serializable representation.'''
+
 
 class Student:
+    '''Represents a student with basic identifying information.'''
     def __init__(self, first_name, last_name, age):
+        '''Initialize a Student with first name, last name, and age.
+
+        Args:
+            first_name (str): Student's first name.
+            last_name (str): Student's last name.
+            age (int): Student's age.'''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        '''Return the dictionary representation of the Student instance.
+
+        Returns:
+            dict: The instance attributes as a dictionary.'''
         return self.__dict__
