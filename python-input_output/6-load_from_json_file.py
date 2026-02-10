6b4fb2b304205220c1f364ff6cc27a9e16efc60f
Antgst <ant.gousset@gmail.com>
2026-02-10 13:04:17 +0100

    fix: add docstrings to T6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-''''''
+'''Module that provides a function to create a
+Python object from a JSON file.'''
 
 import json
 
 
 def load_from_json_file(filename):
-    ''''''
+    '''Create and return a Python object from a JSON file.
+
+    Args:
+        filename (str): Path to the JSON file.
+
+    Returns:
+        object: The Python object represented by the JSON content.'''
     with open(filename, encoding="utf-8") as f:
         return json.load(f)
