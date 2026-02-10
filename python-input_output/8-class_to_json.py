da8d10d9a75423895adeeaa59f908ab30896ab4b
Antgst <ant.gousset@gmail.com>
2026-02-10 13:08:02 +0100

    fix: add dosctrings to T8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+'''Module that provides a function to get a JSON-serializable
+dict description of an object.'''
+
 
 def class_to_json(obj):
+    '''Return the dictionary description of an object for JSON serialization.
+
+    Args:
+        obj (object): An instance of a class with JSON-serializable attributes.
+
+    Returns:
+        dict: The instance attributes of `obj` as a dictionary.'''
     return obj.__dict__
