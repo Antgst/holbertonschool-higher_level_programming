3ace8233614bbee7bf080585ad68cdd6d5027517
Antgst <ant.gousset@gmail.com>
2026-02-10 14:29:44 +0100

    add: resolve T3

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+
+import xml.etree.ElementTree as ET
+
+
+def serialize_to_xml(dictionary, filename):
+
+    root = ET.Element("data")
+
+    for key, value in dictionary.items():
+        child = ET.SubElement(root, str(key))
+        child.text = "" if value is None else str(value)
+
+    tree = ET.ElementTree(root)
+    tree.write(filename, encoding="utf-8", xml_declaration=False)
+
+    def deserialize_from_xml(filename):
+        tree = ET.parse(filename)
+        root = tree.getroot()
+
+        result = {}
+        for child in root:
+            result[child.tag] = "" if child.text is None else child.text
+            return result
