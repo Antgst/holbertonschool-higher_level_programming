abc9200364b241d9c6b93bb055fb3811c578da56
Antgst <ant.gousset@gmail.com>
2026-02-10 10:18:23 +0100

    add: resolve T12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
 
 def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        if i >= 2:
+            prev = triangle[i - 1]
+            for j in range(1, i):
+                row[j] = prev[j - i] + [j]
+        triangle.append(row)
+
+    return triangle
