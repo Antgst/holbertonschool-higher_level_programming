1e15543506a7dafee19b176d294006c9c6426afa
Antgst <ant.gousset@gmail.com>
2026-02-10 10:12:42 +0100

    add: resolve T11

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
 
-def __init__(self, first_name, last_name, age):
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            return {
+                key: value
+                for key, value in self.__dict__.items()
+                if key in attrs
+            }
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
