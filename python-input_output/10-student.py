98d87c636b383a49cd1625e31488734ee603047d
Antgst <ant.gousset@gmail.com>
2026-02-11 14:23:27 +0100

    fix: syntax for T10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-'''Module that defines a Student class with optional JSON attribute filtering.'''
+'''Module that defines a Student class with optional
+JSON attribute filtering.'''
 
 
 class Student:
