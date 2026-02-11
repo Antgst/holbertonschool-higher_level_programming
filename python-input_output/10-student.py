8a5a1661c7c751a9a7670702b8740edc137a4ea4
Antgst <ant.gousset@gmail.com>
2026-02-11 14:13:13 +0100

    edit: syntax T10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -16,18 +16,8 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        '''Return a dictionary representation of the Student instance.
-
-        If attrs is a list of strings, only attributes whose names appear in
-        attrs are included. Otherwise, all instance attributes are returned.
-
-        Args:
-            attrs (list[str] | None): Optional list of attribute
-            names to include.
-
-        Returns:
-            dict: Dictionary of selected (or all) instance attributes.'''
-        if isinstance(attrs, list):
+        '''Return a dictionary representation of the Student instance.'''
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
             return {
                 key: value
                 for key, value in self.__dict__.items()
