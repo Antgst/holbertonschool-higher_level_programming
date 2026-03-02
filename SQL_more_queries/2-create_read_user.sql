0e2edb92f8be32b9df5d17ea125f2fd840f6a26d
Antgst <ant.gousset@gmail.com>
2026-03-02 22:21:22 +0100

    add: resolve T2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 -- Task 2: Create database hbtn_0d_2 and user user_0d_2 with SELECT privilege only
 
 -- Create the database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- Create user user_0d_2@localhost with password if it doesn't exist
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd'
