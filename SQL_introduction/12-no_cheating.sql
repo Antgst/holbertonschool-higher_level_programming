d9edb3e87c31d92f78038609acb78e3469cc5a3c
Antgst <ant.gousset@gmail.com>
2026-03-02 17:45:19 +0100

    fix: edit UPPER of T12

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,6 @@
-update second_table
-set score = '10'
-where name = 'bob';
+-- Task 12: Update Bob's score to 10
+
+-- Set score to 10 for the record with name 'Bob'
+UPDATE second_table
+Set score = 10
+WHERE name = 'Bob';
