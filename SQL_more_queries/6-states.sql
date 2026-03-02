4536259a943957cc639ddf7ae663834364fbc599
Antgst <ant.gousset@gmail.com>
2026-03-02 22:41:30 +0100

    add: resolve T6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,15 @@
+-- Task 6: Create database hbtn_0d_usa and table states
+
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the database
+USE hbtn_0d_usa;
+
+-- Create the table states if it doesn't exist
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (id)
+);
