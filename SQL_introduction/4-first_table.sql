04113204869708bbdf4180b11ab9b4f3a810087e
Antgst <ant.gousset@gmail.com>
2026-03-02 17:38:18 +0100

    fix: edit UPPER T4

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,7 @@
-create table if not exists first_table(
+-- Task 4: Create first_table
+
+-- Create table first_table if it doesn't exist
+CREATE TABLE IF NOT EXISTS first_table(
     id INT,
     name VARCHAR(256)
 );
