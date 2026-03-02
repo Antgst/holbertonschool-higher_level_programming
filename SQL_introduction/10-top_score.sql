73dfc0a8b76d0245085b50a3d150c6369335ebb9
Antgst <ant.gousset@gmail.com>
2026-03-02 17:43:45 +0100

    fix: edit UPPER T10

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,6 @@
-select score, name
-from second_table
-order by score desc;
+-- Task 10: List all records of second_table ordered by score (top first)
+
+-- Display score and name ordered by score descending
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
