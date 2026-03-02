5619a537f0b3bda224768f9ee6656b5880fb76bf
Antgst <ant.gousset@gmail.com>
2026-03-02 17:44:30 +0100

    fix: edit UPPER T11

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,4 +1,7 @@
-select score, name
-from second_table
-where score >= 10
-order by score desc;
+-- Task 11: List all records with score >= 10 ordered by score (top first)
+
+-- Display score and name where score is at least 10
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
