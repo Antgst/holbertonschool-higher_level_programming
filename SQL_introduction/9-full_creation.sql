fd83f227a0b78dd87b68bf5f64ddb912b1893bc9
Antoine Gousset <ant.gousset@gmail.com>
2026-03-02 13:23:52 +0100

    add: resolve T9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -2,11 +2,10 @@ create table if not exists second_table(
     id INT,
     name VARCHAR(256),
     score INT
-)
-
-insert into second_table values(
-1, 'John', 10
-2, 'Alex', 3
-3, 'Bob', 14
-4, 'George', 8
 );
+
+insert into second_table values
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
