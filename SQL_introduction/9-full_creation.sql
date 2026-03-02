ccabafc3241e79d1d86ef847f6280c956ed20cf9
Antgst <ant.gousset@gmail.com>
2026-03-02 17:42:09 +0100

    fix: edit UPPER of T9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,10 +1,14 @@
-create table if not exists second_table(
+-- Task 9: Create second_table and add rows
+
+-- Create table second_table if it doesn't exist
+CREATE TABLE IF NOT EXISTS second_table(
     id INT,
     name VARCHAR(256),
     score INT
 );
 
-insert into second_table values
+-- Insert records into second_table
+INSERT INTO second_table (id, name, score) VALUES
 (1, 'John', 10),
 (2, 'Alex', 3),
 (3, 'Bob', 14),
