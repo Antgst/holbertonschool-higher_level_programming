f8c3044ac7abe4cb0726f990aadfbcef00617257
Antgst <ant.gousset@gmail.com>
2026-03-02 22:42:09 +0100

    fix: resolve T5

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -2,6 +2,7 @@
 
 -- Create the table unique_id if it doesn't exist
 CREATE TABLE IF NOT EXISTS unique_id (
-    id INT DEFAULT 1 UNIQUE,
-    name VARCHAR(256)
+    id INT DEFAULT 1,
+    name VARCHAR(256),
+    UNIQUE (id)
 );
