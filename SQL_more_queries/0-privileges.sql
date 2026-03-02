c7b9c2a0f670dc56b48fa6312ec1cf4ff4d6b1ac
Antgst <ant.gousset@gmail.com>
2026-03-02 20:17:51 +0100

    add: resolve T0

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- Task 0: List all privileges for user_0d_1 and user_0d_2 on localhost
+
+-- Show grants for user_0d_1@localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show grants for user_0d_2@'localhost'
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
