d5b8a2ffbfbbcab7a672a1fb8a3b828835fd5e18
Antgst <ant.gousset@gmail.com>
2026-03-03 16:08:30 +0100

    fix: add description to T12

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,3 +1,6 @@
+-- Task 12: List all shows in hbtn_0d_tvshows without a genre linked.
+
+-- Lists tv_shows.title and tv_show_genres.genre_id for shows with no genre (NULL), sorted by title and genre_id.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
