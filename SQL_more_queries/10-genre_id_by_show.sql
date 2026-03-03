9d066b828daff32815a14d95001065f9f4b9b231
Antgst <ant.gousset@gmail.com>
2026-03-03 10:54:35 +0100

    add: resolve T10

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,8 @@
+-- Task 10: List shows with at least one linked genre
+
+-- Display tv_shows.title and tv_show_genres.genre_id ordered by title and genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
