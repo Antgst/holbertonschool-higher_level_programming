6d265b80fdf571ef7238c9ffa8b0ce49e5c4bbca
Antgst <ant.gousset@gmail.com>
2026-03-03 16:11:40 +0100

    fix: add description to T13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,10 @@
-SELECT tv_genres.name AS genre
+-- Task 13: List all genres and the number of shows linked to each (only genres with at least one show), sorted by number_of_shows DESC.
+
+-- Displays: genre - number_of_shows
+SELECT tv_genres.name AS genre,
 COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY genre
+GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
