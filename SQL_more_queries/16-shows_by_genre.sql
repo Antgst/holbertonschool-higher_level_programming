b876e6de2188483bbf33f3c31602918ab26b1e1b
Antgst <ant.gousset@gmail.com>
2026-03-03 16:21:58 +0100

    add: resolve T16

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- Task 16: List all shows and all genres linked to each show (NULL if no genre).
+
+-- Displays tv_shows.title and tv_genres.name sorted by title ASC, name ASC.
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_shows_genres
+ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title ASC, tv_genre.name ASC;
