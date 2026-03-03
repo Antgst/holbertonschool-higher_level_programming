566f760c88776618cbf9019edff6930271ee896c
Antgst <ant.gousset@gmail.com>
2026-03-03 16:23:18 +0100

    fix: add some missing characters to T16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -3,8 +3,8 @@
 -- Displays tv_shows.title and tv_genres.name sorted by title ASC, name ASC.
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-LEFT JOIN tv_shows_genres
+LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
-ORDER BY tv_shows.title ASC, tv_genre.name ASC;
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
