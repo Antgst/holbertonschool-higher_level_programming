6568f7b179fca534cf6cdb601815e7ac3843066a
Antgst <ant.gousset@gmail.com>
2026-03-04 14:07:13 +0100

    add: resolve T2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     password = sys.argv[2]
     db = sys.argv[3]
     state = sys.argv[4]
+
     conn = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -18,9 +19,22 @@ if __name__ == "__main__":
         db=db,
         charset="utf8"
     )
+
     cursor = conn.cursor()
-    cursor.execute(
+
+    query = (
         "SELECT id, name "
         "FROM states "
         "WHERE name = '{}' "
-    )
+        "ORDER BY id ASC"
+    ).format(state)
+
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    conn.close()
