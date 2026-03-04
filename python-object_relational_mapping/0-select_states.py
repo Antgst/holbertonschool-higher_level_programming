c4608fad7dc1e0697b75ca524855d96e027315c2
Antgst <ant.gousset@gmail.com>
2026-03-04 10:34:17 +0100

    add: add db to test and resolve T0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""List all states from the database and print them ordered by id."""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=db,
+        charset="utf8"
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    conn.close()
