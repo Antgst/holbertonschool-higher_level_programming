397b8b445358a55b438c80b6452efe94aa66d120
Antgst <ant.gousset@gmail.com>
2026-03-04 11:18:06 +0100

    fix: edit the brackets and backslash on T1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -18,9 +18,9 @@ if __name__ == "__main__":
     )
     cursor = conn.cursor()
     cursor.execute(
-        "SELECT id, name " \
-        "FROM states " \
-        "WHERE name LIKE 'N%' " \
+        "SELECT id, name"
+        "FROM states"
+        "WHERE name LIKE 'N%'"
         "ORDER BY id ASC")
     rows = cursor.fetchall()
     for row in rows:
