68db9842d79158566f7c75269717d11409d98fc2
Antgst <ant.gousset@gmail.com>
2026-03-04 09:48:26 +0100

    fix: syntax of T14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -5,7 +5,7 @@ SELECT tv_genres.name
 FROM tv_genres
 JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows 
+JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY TV_genres.name ASC;
