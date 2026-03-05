3ad2279e258181d49c8a8c7d06e8772413e2c403
Antgst <ant.gousset@gmail.com>
2026-03-05 13:28:46 +0100

    add: resolve T11

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Add the State object 'Louisiana' to the database hbtn_0e_6_usa using SQLAlchemy and print the new state's id."""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    url = "mysql+mysqldb://{}:{}@localhost/{}".format(
+        username, password, database
+    )
+
+    engine = create_engine(url, pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+
+    print(new_state.id)
+
+    session.close()
