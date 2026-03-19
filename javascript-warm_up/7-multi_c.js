23750e60e4b819ef08de86b85187a6ea977cfc2f
Antgst <ant.gousset@gmail.com>
2026-03-19 19:01:46 +0100

    fix: change uppercase in T7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 
 const occurences = parseInt(process.argv[2]);
 
-if (isaNan(occurences)) {
+if (isaNaN(occurences)) {
   console.log('Missing number of occurences');
 } else {
   for (let i = 0; i < occurences; i++) {
