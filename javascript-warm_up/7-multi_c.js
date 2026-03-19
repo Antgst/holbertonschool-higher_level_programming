057d7c99637e700a00c3e69b3d667fa5bbf4ec76
Antgst <ant.gousset@gmail.com>
2026-03-19 19:00:06 +0100

    add: resolve T7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const occurences = parseInt(process.argv[2]);
+
+if (isaNan(occurences)) {
+  console.log('Missing number of occurences');
+} else {
+  for (let i = 0; i < occurences; i++) {
+    console.log('C is fun');
+  }
+}
