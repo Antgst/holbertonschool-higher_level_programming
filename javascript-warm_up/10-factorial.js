694b5f4ddfc2a4bc1f2ad3e55444a8e29c3f1b21
Antgst <ant.gousset@gmail.com>
2026-03-19 19:16:18 +0100

    add: resolve T10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+function factorial (n) {
+  const number = parseInt(n);
+  if (isNaN(number) || num <= 1) return 1;
+  return number * factorial(number - 1);
+}
+
+const n = process.argv[2];
+console.log(factorial(n));
