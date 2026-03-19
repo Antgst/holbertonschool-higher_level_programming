0b9d4d846a9462b3e0e78a9066356e0e1541223b
Antgst <ant.gousset@gmail.com>
2026-03-19 19:07:47 +0100

    fix: syntax of T7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 
 const occurrences = parseInt(process.argv[2]);
 
-if (isaNaN(occurrences)) {
+if (isNaN(occurrences)) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < occurrences; i++) {
