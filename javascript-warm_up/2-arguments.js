3aa7c4153b00bc4d241c1bd116e1acbcd6f7d8b7
Antgst <ant.gousset@gmail.com>
2026-03-19 09:52:21 +0100

    add: resolve and test T1

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
 
+if (arguments === 0) { 
+console.log('No argument');
+}
+else if (arguments === 1) {
+console.log('Argument found');
+}
+else {
+console.log('Argument found');
+}
