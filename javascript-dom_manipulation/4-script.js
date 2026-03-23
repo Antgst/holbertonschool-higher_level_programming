223c4cd8828fac2d9b3e6df1114980980896485e
Antgst <ant.gousset@gmail.com>
2026-03-23 12:18:14 +0100

    add: resolve T4

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,8 @@
+const item = document.querySelector('#add_item');
+const list = document.querySelector('.my_list');
+
+item.addEventListener('click', function () {
+  const li = document.createElement('li');
+  li.textContent = "Item";
+  list.append('li')
+});
