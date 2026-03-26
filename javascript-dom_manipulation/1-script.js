5c78e7fac4cd9ffc23549d7c68babb953e011b65
Antoine Gousset <ant.gousset@gmail.com>
2026-03-26 09:17:23 +0100

    Update 1-script.js

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -2,7 +2,7 @@ const header = document.querySelector('header');
 const red_header = document.querySelector('#red_header');
 
 red_header.addEventListener('click', function () {
-  header.style.color = "#FF0000";
+  header.style.color = '#FF0000';
 });
 
 /*
