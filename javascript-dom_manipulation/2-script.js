616951849b8cffe2d9f13fd93e063598363e334f
Antoine Gousset <ant.gousset@gmail.com>
2026-03-26 09:18:56 +0100

    Fix syntax error in red_header click event

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -2,5 +2,5 @@ const header = document.querySelector('header');
 const red_header = document.querySelector('#red_header');
 
 red_header.addEventListener('click', function () {
-  header.classList.add('red')
+  header.classList.add('red');
 });
