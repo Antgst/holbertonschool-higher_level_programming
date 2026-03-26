d71241bcffbf5a84d3fe5638103773bb411f6848
Antoine Gousset <ant.gousset@gmail.com>
2026-03-26 09:20:49 +0100

    Fix syntax error in header update

--- javascript-dom_manipulation/5-script.js
+++ javascript-dom_manipulation/5-script.js
@@ -2,5 +2,5 @@ const header = document.querySelector('header');
 const updateHeader = document.querySelector('#update_header');
 
 updateHeader.addEventListener('click', function () {
-  header.textContent = 'New Header!!!'
+  header.textContent = 'New Header!!!';
 });
