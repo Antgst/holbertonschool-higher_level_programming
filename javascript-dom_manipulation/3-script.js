6ba722b5d241a890181bf15c62824190cdc35fb3
Antoine Gousset <ant.gousset@gmail.com>
2026-03-26 09:19:50 +0100

    Update 3-script.js

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,7 +1,7 @@
 const header = document.querySelector('header');
-const toogle = document.querySelector('#toggle_header');
+const toggle = document.querySelector('#toggle_header');
 
-toogle.addEventListener('click', function () {
+toggle.addEventListener('click', function () {
   if (header.classList.contains('red')) {
     header.classList.remove('red');
     header.classList.add('green');
